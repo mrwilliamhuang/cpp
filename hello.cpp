31c1b89cb07c25ee97d09fdf8313a11397e5f208
JimmyHuang037 <huangchengliang037@gmail.com>
2025-07-18 22:21:03 +0800

    your message

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,6 @@
 // 主函数，程序的入口点
 int main() {
     // 输出 "Hello, World!" 到标准输出
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!i'm huangchengliang!!!!!!!!!!!!!!good evening!!!!" << std::endl;
     return 0;
 }
